ec06f781ce8881641de0098af816f11151201e7e
Joona Myllys <jcmyllys@gmail.com>
2019-06-29 15:24:37 +0300

    first commit

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tracklist from './Tracklist/Tracklist';
+class Playlist extends React.Component{
+    render(){
+        return (<div className="Playlist">
+        <input value="New Playlist"/>
+        <Tracklist />
+        <button class="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>)
+    }
+}
+export default Playlist;
